3a2af3b0ab869dbccd35f6c5866983fbe124468a
dvlp-omer <tahafdn231@gmail.com>
2025-10-21 09:55:04 +0300

    Add files via upload

---
+++ op.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main(){
+    /* int i;
+    for (i = 1; i <100; i++)
+    {
+        if((i%2)== 0)
+        printf("%d, ", i);
+    }  
+    */
+  
+    /*
+  
+  
+    unsigned int x;
+
+    for (x = 1; x < 5; x++)
+    {
+        if (x == 5)
+        {
+            break;
+        }
+        printf("%d\n", x);
+    }
+        printf("\nx in son degeri : %d", x);
+   */
+   /*
+   int x;
+   for ( x = 1; x < 100; x++)
+{   
+    if((x ==5)) {
+        continue;
+    }
+}
+printf("%u", x);   
+*/
+//1 ile 100 arasındaki sayilarin toplamını ,ortalamasını hesaplayan C kodunu yazınız.
+
+/*
+int x,sum=0,;
+float ortalama;
+for (x=1; x<=100; x++)
+{
+    sum=sum+x;
+}
+printf("1 ile 100 arasındaki sayıların toplamı: %d", sum);  
+    ortalama = sum/100;
+   printf("\n1 ile 100 arasındaki sayıların toplamının ortalaması %d",ortalama);
+return 0;
+*/
+
+
+
+
+
+
+
+return 0;
+}
